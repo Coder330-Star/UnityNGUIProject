4e44c5cdf8d61a691dc059d590490d6b6304899e
wangxingyu <614486626@qq.com>
2021-09-29 17:22:39 +0800

    技能冷却

---
+++ Assets/Scripts/Skill_CD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_CD : MonoBehaviour
+{    
+    public UISprite maskSp;//冷却遮罩
+
+    private float coldTime = 2f;
+    private bool isCoding = false;//表public
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && !isCoding)
+        {
+            maskSp.fillAmount = 1;
+            isCoding = true;
+        }
+
+        if (isCoding)
+        {
+            maskSp.fillAmount -= (1f / coldTime) * Time.deltaTime;
+            if (maskSp.fillAmount <= 0.05f)
+            {
+                maskSp.fillAmount = 0;
+                isCoding = false;
+
+
+            }
+        }
+    }
+}

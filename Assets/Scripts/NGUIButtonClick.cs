cccc5da270c4700f2200665e51bde19b8979ba00
Home <614486626@qq.com>
2021-09-29 11:15:23 +0800

    工程创建

---
+++ Assets/Scripts/NGUIButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NGUIButtonClick : MonoBehaviour
+{
+    public void OnLabelButtonClick() 
+    {
+        Debug.Log("Label");
+    }
+
+    public void OnSpriteButtonClick() 
+    {
+        Debug.Log("Sprite");
+    }
+}

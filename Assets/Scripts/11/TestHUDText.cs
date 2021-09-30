3e3afe11793ebe68b2611fb750c3aa1e2a500a81
wangxingyu <614486626@qq.com>
2021-09-30 17:56:53 +0800

    课程完结

---
+++ Assets/Scripts/11/TestHUDText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHUDText : MonoBehaviour
+{
+    public HUDText hud;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hud.Add(-10, Color.red, 0.1f);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            hud.Add(+10, Color.green, 0.1f);
+        }
+    }
+}

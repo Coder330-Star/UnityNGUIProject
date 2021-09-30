5a933333682bdf1a8907e82a9cfe9e8f8c093797
wangxingyu <614486626@qq.com>
2021-09-30 17:02:53 +0800

    背包系统的设计与开发

---
+++ Assets/Scripts/09/Knapsack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knapsack : MonoBehaviour
+{
+    public GameObject[] cells;
+    public string[] equipmentsName;
+
+    public GameObject item;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Pickup();
+        }
+    }
+
+
+    public void Pickup()
+    {
+        int index = Random.Range(0, equipmentsName.Length);
+        string name = equipmentsName[index];
+        bool isFind = false;//表示找到了相同的格子 
+
+
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i].transform.childCount >0)
+            {
+                var grid = cells[i].GetComponentInChildren<MyDragAndDrop>();
+                if (grid.sp.spriteName == name)//表示找到了
+                {
+                    isFind = true;
+                    grid.AddCount(1);
+                    break;
+                }
+            }
+        }
+
+        if (!isFind)
+        {
+            for (int i = 0; i < cells.Length; i++)
+            {
+                if (cells[i].transform.childCount == 0)
+                {
+                    //当前位置没有物品
+                    //添加物品
+                    GameObject go = NGUITools.AddChild(cells[i], item);
+                    go.GetComponent<UISprite>().spriteName = name;
+                    go.transform.localPosition = Vector3.zero;
+
+                    break;
+                }
+            }
+        }
+        
+
+        
+    }
+}
---
+++ Assets/Scripts/09/MyDragAndDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDragAndDrop : UIDragDropItem
+{
+
+    public UISprite sp;
+    public UILabel label;
+
+    private int count = 1;
+
+    public void AddCount(int num = 1) 
+    {
+        count += num;
+        label.text = count.ToString();
+    }
+
+
+    protected override void OnDragDropRelease(GameObject surface)
+    {
+        base.OnDragDropRelease(surface);
+
+
+        if (surface.tag=="Cell")
+        {
+
+            transform.parent = surface.transform;
+            transform.localPosition = Vector3.zero;
+        }
+        else if (surface.tag == "Item")
+        {
+            var parent = surface.transform.parent;
+            surface.transform.parent = transform.parent;
+            surface.transform.localPosition = Vector3.zero;
+
+            transform.parent = parent;
+            transform.localPosition = Vector3.zero;
+        }
+    }
+}
